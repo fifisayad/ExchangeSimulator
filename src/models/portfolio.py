c84231a9bc3b3da86b52f55f277756d162e44f30
fifi <sayad.mehrdad@gmail.com>
2025-07-09 16:49:08 +0330

    fix: rename table, fix constraints and relationships

--- src/models/portfolio.py
+++ src/models/portfolio.py
@@ -2,16 +2,13 @@ from typing import List
 from fifi import DatetimeDecoratedBase
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from .balance import Balance
-from .order import Order
-
 
 class Portfolio(DatetimeDecoratedBase):
-    __tablename__ = "portfolio"
+    __tablename__ = "portfolios"
     # columns
     name: Mapped[str] = mapped_column(unique=True, nullable=False)
     # relationships
-    orders: Mapped[List[Order]] = relationship("order", back_populates="portfolio")
-    balances: Mapped[List[Balance]] = relationship(
-        "balance", back_populates="portfolio"
+    orders: Mapped[List["Order"]] = relationship("Order", back_populates="portfolio")
+    balances: Mapped[List["Balance"]] = relationship(
+        "Balance", back_populates="portfolio"
     )
