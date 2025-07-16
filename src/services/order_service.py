1d94d3c2e7b405c087481eb2e61ff0cf9066dbac
fifi <sayad.mehrdad@gmail.com>
2025-07-16 16:12:12 +0330

    feat: add fee calculation method

--- src/services/order_service.py
+++ src/services/order_service.py
@@ -1,4 +1,7 @@
-from typing import List
+from typing import List, Union
+
+from src.enums.order_side import OrderSide
+from src.enums.order_type import OrderType
 
 from ..enums.order_status import OrderStatus
 from ..repository import OrderRepository
@@ -11,3 +14,27 @@ class OrderService:
 
     async def get_open_orders(self) -> List[Order]:
         return await self.order_repo.get_all_order(status=OrderStatus.ACTIVE)
+
+    async def fee_calc(self, orders: Union[Order, List[Order]]) -> None:
+        orders_list: List[Order] = (
+            [orders] if type(orders) == Order else orders
+        )  # type: ignore
+        for order in orders_list:
+            order_total = order.size * order.price
+            if order.market.is_perptual():
+                if order.type == OrderType.LIMIT:
+                    order.fee = order.portfolio.perp_maker_fee * order_total
+                elif order.type == OrderType.MARKET:
+                    order.fee = order.portfolio.perp_taker_fee * order_total
+            else:
+                if order.type == OrderType.LIMIT:
+                    if order.side == OrderSide.BUY:
+                        order.fee = order.size * order.portfolio.spot_maker_fee
+                    else:
+                        order.fee = order_total * order.portfolio.spot_maker_fee
+                elif order.type == OrderType.MARKET:
+                    if order.side == OrderSide.BUY:
+                        order.fee = order.size * order.portfolio.spot_taker_fee
+                    else:
+                        order.fee = order_total * order.portfolio.spot_taker_fee
+        # TODO:implement merge functionality for changes on the db models
