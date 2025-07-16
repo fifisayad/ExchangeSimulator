50e00436cbfe484e4b61c9b270bd1cfb5b4c4aa2
fifi <sayad.mehrdad@gmail.com>
2025-07-16 13:17:55 +0330

    feat: add active markets setting variable

--- src/common/settings.py
+++ src/common/settings.py
@@ -1,8 +1,18 @@
+from typing import Annotated
 from dotenv import load_dotenv
-from pydantic_settings import BaseSettings
+from pydantic import field_validator
+from pydantic_settings import BaseSettings, NoDecode
+from ..enums.market import Market
 
 
 class Setting(BaseSettings):
     def __init__(self):
-        super().__init__()
         load_dotenv()
+        super().__init__()
+
+    active_markets: Annotated[list[Market], NoDecode]
+
+    @field_validator("active_markets", mode="before")
+    @classmethod
+    def decode_active_markets(cls, v: str) -> list[Market]:
+        return [Market[x] for x in v.split(",")]
