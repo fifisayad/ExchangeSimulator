6ee89f9a8c809b695e7a1f772f2bf667217f7633
fifi <sayad.mehrdad@gmail.com>
2025-07-18 19:03:44 +0330

    feat: add test for balance get all method

---
+++ tests/repository/test_balance_repository.py
@@ -0,0 +1,32 @@
+from fifi import DecoratedBase
+import pytest
+
+from src.repository import PortfolioRepository
+from src.repository import BalanceRepository
+from src.repository import OrderRepository
+from src.enums.asset import Asset
+from tests.repository.materials import *
+
+
+@pytest.mark.asyncio
+class TestBalanceRepository:
+    portfilio_repo = PortfolioRepository()
+    order_repo = OrderRepository()
+    balance_repo = BalanceRepository()
+
+    async def test_get_all_balances(
+        self, database_provider_test, portfolio_factory, balance_factory_for_portfolios
+    ):
+        portfolios_schema = [portfolio_factory() for i in range(5)]
+        portfolios = await self.portfilio_repo.create_many(
+            data=portfolios_schema, return_models=True
+        )
+
+        for portfolio in portfolios:  # type: ignore
+            balances_schema = balance_factory_for_portfolios(portfolio.id)
+            is_created = await self.balance_repo.create_many(data=balances_schema)
+            assert is_created == True
+
+        balances = await self.balance_repo.get_all_balances()
+
+        assert len(balances) == Asset.__len__() * 5
