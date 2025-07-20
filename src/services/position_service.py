b016319c231824223d6409fad58251b7e0f02add
fifi <sayad.mehrdad@gmail.com>
2025-07-20 16:04:43 +0330

    feat: add get open positions with hashmap structure

--- src/services/position_service.py
+++ src/services/position_service.py
@@ -1,3 +1,7 @@
+from typing import Dict, List
+
+from src.enums.position_status import PositionStatus
+from src.models.position import Position
 from ..repository import PositionRepository
 
 
@@ -5,3 +9,14 @@ class PositionService:
 
     def __init__(self) -> None:
         self.position_repo = PositionRepository()
+
+    async def get_open_positions(self) -> List[Position]:
+        return await self.position_repo.get_all_positions(status=PositionStatus.OPEN)
+
+    async def get_open_positions_hashmap(self) -> Dict[str, Position]:
+        open_positions = await self.get_open_positions()
+        positions = dict()
+        # unique hash for this is {market}_{portfolio_id}
+        for position in open_positions:
+            positions[f"{position.market}_{position.portfolio_id}"] = position
+        return positions
