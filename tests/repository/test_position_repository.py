9f045c224d5f29396d94ec8f31d3ec8e9acb5ab3
fifi <sayad.mehrdad@gmail.com>
2025-07-21 15:01:44 +0330

    feat: add test for getting all positions with open status

--- tests/repository/test_position_repository.py
+++ tests/repository/test_position_repository.py
@@ -1,5 +1,6 @@
 import pytest
 
+from src.models import Position
 from src.repository import PositionRepository
 from tests.repository.materials import *
 
@@ -22,3 +23,25 @@ class TestpositionRepository:
 
         for position in got_positions:
             assert position.id in positions_id_set
+
+    async def test_get_all_positions_with_status(
+        self, database_provider_test, position_factory
+    ):
+        positions_schemas: List[PositionSchema] = position_factory(count=100)
+        positions: List[Position] = await self.position_repo.create_many(
+            data=positions_schemas, return_models=True
+        )
+
+        active_positions_id_set = set()
+        for position in positions:
+            if position.status == PositionStatus.OPEN:
+                active_positions_id_set.add(position.id)
+
+        LOGGER.info(f"active positions counts: {len(active_positions_id_set)}")
+
+        got_positions = await self.position_repo.get_all_positions(
+            status=PositionStatus.OPEN
+        )
+
+        for position in got_positions:
+            assert position.id in active_positions_id_set
