85186c42de8fa8b4f9abf5b7a50734d3c155b7c0
fifi <sayad.mehrdad@gmail.com>
2025-07-21 15:07:14 +0330

    feat: add test for getting all positions with side filter

--- tests/repository/test_position_repository.py
+++ tests/repository/test_position_repository.py
@@ -45,3 +45,25 @@ class TestpositionRepository:
 
         for position in got_positions:
             assert position.id in active_positions_id_set
+
+    async def test_get_all_positions_with_side(
+        self, database_provider_test, position_factory
+    ):
+        positions_schemas: List[PositionSchema] = position_factory(count=100)
+        positions: List[Position] = await self.position_repo.create_many(
+            data=positions_schemas, return_models=True
+        )
+
+        long_positions_id_set = set()
+        for position in positions:
+            if position.side == PositionSide.LONG:
+                long_positions_id_set.add(position.id)
+
+        LOGGER.info(f"long positions counts: {len(long_positions_id_set)}")
+
+        got_positions = await self.position_repo.get_all_positions(
+            side=PositionSide.LONG
+        )
+
+        for position in got_positions:
+            assert position.id in long_positions_id_set
