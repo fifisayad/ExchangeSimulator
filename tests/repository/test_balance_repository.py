02620961122f99b1ec50bedf321deb4b16e12ce1
fifi <sayad.mehrdad@gmail.com>
2025-07-21 12:36:01 +0330

    feat: add unit test for get portfolio asset

--- tests/repository/test_balance_repository.py
+++ tests/repository/test_balance_repository.py
@@ -30,3 +30,21 @@ class TestBalanceRepository:
         balances = await self.balance_repo.get_all_balances()
 
         assert len(balances) == Asset.__len__() * 5
+
+    async def test_get_portfolio_asset(
+        self, database_provider_test, portfolio_factory, balance_factory_for_portfolios
+    ) -> None:
+        portfolio_schema = portfolio_factory()
+        portfolio = await self.portfilio_repo.create(data=portfolio_schema)
+
+        balances_schema = balance_factory_for_portfolios(portfolio.id)
+        balances = await self.balance_repo.create_many(
+            data=balances_schema, return_models=True
+        )
+
+        for balance in balances:
+            got_balance = await self.balance_repo.get_portfolio_asset(
+                portfolio_id=balance.portfolio_id, asset=balance.asset
+            )
+            assert got_balance is not None
+            assert got_balance.to_dict() == balance.to_dict()
