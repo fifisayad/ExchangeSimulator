c38a68188f470cea6667a9cfc6da1ce2fbb9fb96
fifi <sayad.mehrdad@gmail.com>
2025-07-21 10:07:51 +0330

    feat: add open new position

--- src/services/position_service.py
+++ src/services/position_service.py
@@ -7,12 +7,15 @@ from ..enums.position_status import PositionStatus
 from ..models import Order, Position
 from ..repository import PositionRepository
 from ..schemas import PositionSchema
+from ..helpers.position_helpers import PositionHelpers
+from .balance_service import BalanceService
 
 
 class PositionService:
 
     def __init__(self) -> None:
         self.position_repo = PositionRepository()
+        self.balance_service = BalanceService()
 
     async def get_open_positions(self) -> List[Position]:
         return await self.position_repo.get_all_positions(status=PositionStatus.OPEN)
@@ -30,11 +33,26 @@ class PositionService:
 
     async def create_position_by_order(self, order: Order) -> Position:
         position = PositionSchema()
-        if order.side == OrderSide.BUY:
-            position.side = PositionSide.LONG
+        position.entry_price = order.price
+        position.portfolio_id = order.portfolio_id
+        position.market = order.market
+        position.size = order.size
+        position.side = PositionHelpers.get_position_side_with_order(order)
+        leverage = await self.balance_service.get_portfolio_asset_leverage(
+            portfolio_id=position.portfolio_id,
+            asset=order.market.get_payment_asset_enum(order.side),
+        )
+        if leverage:
+            position.leverage = leverage
         else:
-            position.side = PositionSide.SHORT
-        # TODO:add logic for opening new position
+            # just in impossible case ;)
+            position.leverage = 1
+        position.lqd_price = PositionHelpers.lqd_price_calc(
+            entry_price=order.price, leverage=position.leverage, side=position.side
+        )
+        position.margin = PositionHelpers.margin_calc(
+            position.size, position.leverage, position.entry_price
+        )
         return await self.position_repo.create(position)
 
     async def liquid_position(self, position: Position) -> None:
