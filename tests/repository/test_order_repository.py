5869e43355d428913a6a407e10d8f9dd617f8faf
fifi <sayad.mehrdad@gmail.com>
2025-07-21 13:12:58 +0330

    feat: add test for get all orders with status

--- tests/repository/test_order_repository.py
+++ tests/repository/test_order_repository.py
@@ -25,3 +25,25 @@ class TestOrderRepository:
 
         for order in got_orders:
             assert order.id in orders_id_set
+
+    async def test_get_all_orders_with_status(
+        self, database_provider_test, order_factory
+    ):
+        order_schemas: List[OrderSchema] = order_factory(count=100)
+        orders = await self.order_repo.create_many(
+            data=order_schemas, return_models=True
+        )
+        active_orders_id_set = set()
+        for order in orders:
+            if order.status == OrderStatus.ACTIVE:
+                active_orders_id_set.add(order.id)
+
+        LOGGER.info(
+            f"[{self.__class__.__name__}]: active orders count: {len(active_orders_id_set)}"
+        )
+        got_orders = await self.order_repo.get_all_order(status=OrderStatus.ACTIVE)
+
+        assert len(got_orders) == len(active_orders_id_set)
+
+        for order in got_orders:
+            assert order.id in active_orders_id_set
