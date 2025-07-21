47e7f62df37ae9e8b1b5bf151f1d8799a512ff8f
fifi <sayad.mehrdad@gmail.com>
2025-07-21 10:32:04 +0330

    feat: add burned attr for burning process

--- src/services/balance_service.py
+++ src/services/balance_service.py
@@ -22,14 +22,15 @@ class BalanceService:
             return asset_balance.leverage
 
     async def burn_balance(
-        self, portfolio_id: str, asset: Asset, burn_qty: float
+        self, portfolio_id: str, asset: Asset, burned_qty: float
     ) -> bool:
         asset_balance = await self.balance_repo.get_portfolio_asset(
             portfolio_id=portfolio_id, asset=asset
         )
         if asset_balance:
-            asset_balance.frozen -= burn_qty
-            asset_balance.quantity -= burn_qty
+            asset_balance.frozen -= burned_qty
+            asset_balance.quantity -= burned_qty
+            asset_balance.burned = burned_qty
             if asset_balance.quantity > 0 and asset_balance.frozen >= 0:
                 await self.balance_repo.update_entity(asset_balance)
                 return True
