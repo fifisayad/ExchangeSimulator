4716ec20256f31b3ff5cfa195193d6d8f0f70e96
fifi <sayad.mehrdad@gmail.com>
2025-07-21 14:52:06 +0330

    feat: add position status

--- src/schemas/position_schema.py
+++ src/schemas/position_schema.py
@@ -1,13 +1,15 @@
 from pydantic import BaseModel
 
-from src.enums.market import Market
-from src.enums.position_side import PositionSide
+from ..enums.market import Market
+from ..enums.position_side import PositionSide
+from ..enums.position_status import PositionStatus
 
 
 class PositionSchema(BaseModel):
     portfolio_id: str = ""
     market: Market = Market.BTCUSD_PERP
     side: PositionSide = PositionSide.LONG
+    status: PositionStatus = PositionStatus.OPEN
     entry_price: float = 0
     lqd_price: float = 0
     size: float = 0
