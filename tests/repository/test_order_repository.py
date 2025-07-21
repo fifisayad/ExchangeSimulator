1f1d9c1b5d5c7b8620c0a16c753f36a9cce9e482
fifi <sayad.mehrdad@gmail.com>
2025-07-21 14:12:46 +0330

    feat: add test for getting filled orders with from update time

--- tests/repository/test_order_repository.py
+++ tests/repository/test_order_repository.py
@@ -1,3 +1,4 @@
+from fifi.helpers.get_current_time import GetCurrentTime
 import pytest
 
 from src.models.order import Order
@@ -39,9 +40,7 @@ class TestOrderRepository:
             if order.status == OrderStatus.ACTIVE:
                 active_orders_id_set.add(order.id)
 
-        LOGGER.info(
-            f"[{self.__class__.__name__}]: active orders count: {len(active_orders_id_set)}"
-        )
+        LOGGER.info(f"active orders count: {len(active_orders_id_set)}")
         got_orders = await self.order_repo.get_all_order(status=OrderStatus.ACTIVE)
 
         assert len(got_orders) == len(active_orders_id_set)
@@ -59,10 +58,36 @@ class TestOrderRepository:
             if order.status == OrderStatus.FILLED and order.market.is_perptual():
                 filled_perp_orders_id_set.add(order.id)
 
-        LOGGER.info(
-            f"[{self.__class__.__name__}]: filled perp orders count: {len(filled_perp_orders_id_set)}"
-        )
+        LOGGER.info(f"filled perp orders count: {len(filled_perp_orders_id_set)}")
         got_orders = await self.order_repo.get_filled_perp_orders()
+
+        assert len(got_orders) == len(filled_perp_orders_id_set)
+
+        for order in got_orders:
+            assert order.id in filled_perp_orders_id_set
+
+    async def test_get_filled_perp_orders_with_update_from(
+        self, database_provider_test, order_factory
+    ):
+        order_schemas: List[OrderSchema] = order_factory(count=1000)
+        orders: List[Order] = await self.order_repo.create_many(
+            data=order_schemas, return_models=True
+        )
+        filled_perp_orders_id_set = set()
+        update_time = GetCurrentTime().get()
+        for order in orders:
+            if order.status == OrderStatus.FILLED and order.market.is_perptual():
+                if random.random() > 0.5:
+                    filled_perp_orders_id_set.add(order.id)
+                    order.price = random.random()
+                    await self.order_repo.update_entity(order)
+
+        LOGGER.info(
+            f"updated filled perp orders count: {len(filled_perp_orders_id_set)}"
+        )
+        got_orders = await self.order_repo.get_filled_perp_orders(
+            from_update_time=update_time
+        )
 
         assert len(got_orders) == len(filled_perp_orders_id_set)
 
