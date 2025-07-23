850fb62bfbfc068e376270e9f651654e4cc4c5f8
fifi <sayad.mehrdad@gmail.com>
2025-07-23 11:40:39 +0330

    feat: update by name

--- src/services/portfolio_service.py
+++ src/services/portfolio_service.py
@@ -1,4 +1,6 @@
 from typing import Optional
+
+from ..schemas.portfolio_schema import PortfolioSchema
 from ..models import Portfolio
 from .service import Service
 from ..repository import PortfolioRepository
@@ -14,3 +16,6 @@ class PortfolioService(Service):
 
     async def read_by_name(self, name: str) -> Optional[Portfolio]:
         return await self.repo.get_by_name(name=name)
+
+    async def update_by_name(self, name: str, data: PortfolioSchema) -> Portfolio:
+        return await self.repo.update_by_id(data=data, id_=name, column="name")
