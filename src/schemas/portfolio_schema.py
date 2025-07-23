570fdd43ba4a5b822624d45244ed4bedf4358ab1
fifi <sayad.mehrdad@gmail.com>
2025-07-23 10:41:02 +0330

    feat: add portfolio schema

--- src/schemas/portfolio_schema.py
+++ src/schemas/portfolio_schema.py
@@ -1,5 +1,13 @@
 from pydantic import BaseModel
+from ..common.settings import Setting
 
 
 class PortfolioSchema(BaseModel):
     name: str
+    spot_taker_fee: float = Setting().DEFAULT_SPOT_TAKER_FEE
+    spot_maker_fee: float = Setting().DEFAULT_SPOT_MAKER_FEE
+    perp_taker_fee: float = Setting().DEFAULT_PERP_TAKER_FEE
+    perp_maker_fee: float = Setting().DEFAULT_PERP_MAKER_FEE
+
+    class Config:
+        orm_mode = True
