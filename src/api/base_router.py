ccd822047e7a4c0497796bc66753192a702f4ab8
fifi <sayad.mehrdad@gmail.com>
2025-07-23 10:41:23 +0330

    feat: add v1 router

--- src/api/base_router.py
+++ src/api/base_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, FastAPI
 from contextlib import asynccontextmanager
 from ..common.settings import Setting
+from .v1.router import router as router_v1
 
 
 setting = Setting()
@@ -16,3 +17,6 @@ async def lifespan(app: FastAPI):
 base_router = APIRouter(
     prefix=f"/{setting.API_VERSION}", tags=["ExchangeAPIs"], lifespan=lifespan
 )
+
+
+base_router.include_router(router=router_v1)
