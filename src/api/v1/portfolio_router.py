2bd2a711380706de25357731c56220e37eb2cb7d
fifi <sayad.mehrdad@gmail.com>
2025-07-23 11:27:50 +0330

    feat: add post method for portfolio creation

--- src/api/v1/portfolio_router.py
+++ src/api/v1/portfolio_router.py
@@ -3,7 +3,7 @@ from contextlib import asynccontextmanager
 
 from .deps import get_portfolio_service
 from ...services import PortfolioService
-from ...schemas.portfolio_schema import PortfolioResponseSchema
+from ...schemas.portfolio_schema import PortfolioResponseSchema, PortfolioSchema
 
 
 @asynccontextmanager
@@ -23,11 +23,27 @@ async def get_portfolio(
     portfolio_service: PortfolioService = Depends(get_portfolio_service),
 ):
     if id:
-        portfolio = portfolio_service.read_by_id(id=id)
+        portfolio = await portfolio_service.read_by_id(id=id)
         if portfolio:
             return portfolio
     if name:
-        portfolio = portfolio_service.read_by_name(name=name)
+        portfolio = await portfolio_service.read_by_name(name=name)
         if portfolio:
             return portfolio
     raise HTTPException(status_code=404, detail="portfolio not found")
+
+
+@router.post("", response_model=PortfolioResponseSchema)
+async def create_portfolio(
+    portfolio: PortfolioSchema,
+    portfolio_service: PortfolioService = Depends(get_portfolio_service),
+):
+    try:
+        existed_portfolio = await portfolio_service.read_by_name(portfolio.name)
+        if existed_portfolio:
+            raise HTTPException(
+                status_code=400, detail=f"the portfolio {portfolio.name=} is existed"
+            )
+        return await portfolio_service.create(data=portfolio)
+    except Exception as ex:
+        raise HTTPException(status_code=400, detail=str(ex))
--- src/schemas/portfolio_schema.py
+++ src/schemas/portfolio_schema.py
@@ -4,6 +4,10 @@ from ..common.settings import Setting
 
 class PortfolioSchema(BaseModel):
     name: str
+    spot_taker_fee: float = Setting().DEFAULT_SPOT_TAKER_FEE
+    spot_maker_fee: float = Setting().DEFAULT_SPOT_MAKER_FEE
+    perp_taker_fee: float = Setting().DEFAULT_PERP_TAKER_FEE
+    perp_maker_fee: float = Setting().DEFAULT_PERP_MAKER_FEE
 
 
 class PortfolioResponseSchema(BaseModel):
