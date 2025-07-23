14f9ca9f6a68e2c1053b3f946fbea996bdd8535e
fifi <sayad.mehrdad@gmail.com>
2025-07-23 12:04:27 +0330

    feat: add factory method for balance service

--- src/api/v1/deps.py
+++ src/api/v1/deps.py
@@ -1,5 +1,9 @@
-from ...services import PortfolioService
+from ...services import PortfolioService, BalanceService
 
 
 def get_portfolio_service() -> PortfolioService:
     return PortfolioService()
+
+
+def get_balance_service() -> BalanceService:
+    return BalanceService()
