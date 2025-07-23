6641803f8f5f1645d64e9f755dc084b5ec48063c
fifi <sayad.mehrdad@gmail.com>
2025-07-23 10:42:15 +0330

    feat: add v1 router py

---
+++ src/api/v1/router.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, FastAPI
+from contextlib import asynccontextmanager
+from .portfolio_router import router as portfolio_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # initialize
+    yield
+    # cleanup
+
+
+router = APIRouter(prefix="/v1", tags=["v1"], lifespan=lifespan)
+
+
+router.include_router(portfolio_router)
