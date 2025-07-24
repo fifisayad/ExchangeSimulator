cc5d2548e103408f83fb5d0d728b5355ea32c089
fifi <sayad.mehrdad@gmail.com>
2025-07-24 11:41:58 +0330

    feat: add leverage repository

--- src/repository/__init__.py
+++ src/repository/__init__.py
@@ -3,9 +3,11 @@ __all__ = [
     "OrderRepository",
     "PortfolioRepository",
     "PositionRepository",
+    "LeverageRepository",
 ]
 
 from .order_repository import OrderRepository
 from .portfolio_repository import PortfolioRepository
 from .balance_repository import BalanceRepository
 from .position_repository import PositionRepository
+from .leverage_repository import LeverageRepository
