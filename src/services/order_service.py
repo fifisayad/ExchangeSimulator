661c46955aa51ed16a101ced12d5c4c3af2b13bb
fifi <sayad.mehrdad@gmail.com>
2025-07-24 13:41:02 +0330

    wip: add order creation method interfaces

--- src/services/order_service.py
+++ src/services/order_service.py
@@ -1,12 +1,15 @@
 from datetime import datetime
 from typing import List, Optional, Union
 
+from ..common.exceptions import NotEnoughBalance
+from ..enums.market import Market
 from ..enums.order_side import OrderSide
 from ..enums.order_type import OrderType
 from .service import Service
 from ..enums.order_status import OrderStatus
 from ..repository import OrderRepository
 from ..models import Order
+from ..schemas import OrderSchema
 
 
 # TODO: REFACTOR get orders with filter and make it flexible
@@ -84,3 +87,69 @@ class OrderService(Service):
 
     async def read_orders_by_portfolio_id(self, portfolio_id: str) -> List[Order]:
         return await self.repo.get_entities_by_portfolio_id(portfolio_id=portfolio_id)
+
+    async def check_lock_balance_for_spot_order(
+        self,
+        portfolio_id: str,
+        market: Market,
+        price: float,
+        size: float,
+        side: OrderSide,
+        order_type: OrderType,
+    ) -> bool:
+        return False
+
+    async def check_lock_balance_for_perp_order(
+        self,
+        portfolio_id: str,
+        market: Market,
+        price: float,
+        size: float,
+        side: OrderSide,
+        order_type: OrderType,
+    ) -> bool:
+        return False
+
+    async def create_order(
+        self,
+        portfolio_id: str,
+        market: Market,
+        price: float,
+        size: float,
+        order_type: OrderType,
+        side: OrderSide,
+    ) -> Optional[Order]:
+        order = None
+        is_ok = False
+        if market.is_perptual():
+            is_ok = await self.check_lock_balance_for_perp_order(
+                portfolio_id=portfolio_id,
+                market=market,
+                price=price,
+                size=size,
+                order_type=order_type,
+                side=side,
+            )
+        else:
+            is_ok = await self.check_lock_balance_for_spot_order(
+                portfolio_id=portfolio_id,
+                market=market,
+                price=price,
+                size=size,
+                order_type=order_type,
+                side=side,
+            )
+        if not is_ok:
+            raise NotEnoughBalance("")
+        order_fee = await self.fee_calc(order)
+        order_schema = OrderSchema(
+            portfolio_id=portfolio_id,
+            market=market,
+            price=price,
+            fee=order_fee,
+            size=size,
+            side=side,
+            type=order_type,
+        )
+        order = await self.create(data=order_schema)
+        return order
