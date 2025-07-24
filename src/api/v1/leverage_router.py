15b5832de24e6d3c81f81a5d9c6223adee37e65f
fifi <sayad.mehrdad@gmail.com>
2025-07-24 12:12:13 +0330

    feat: add get endpoint for leverage

---
+++ src/api/v1/leverage_router.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Depends, FastAPI, HTTPException
+from contextlib import asynccontextmanager
+
+from ...schemas.leverage_schema import LeverageSchema, LeverageReadSchema
+from .deps import get_leverage_service
+from ...services import LeverageService
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # initialize
+    yield
+    # cleanup
+
+
+leverage_router = APIRouter(prefix="/leverage", tags=["Leverage"], lifespan=lifespan)
+
+
+@leverage_router.get("", response_model=LeverageSchema)
+async def get_balance(
+    leverage_query: LeverageReadSchema,
+    leverage_service: LeverageService = Depends(get_leverage_service),
+):
+    leverage = await leverage_service.get_portfolio_market_leverage(
+        portfolio_id=leverage_query.portfolio_id, market=leverage_query.market
+    )
+    if leverage:
+        return leverage
+    raise HTTPException(status_code=404, detail="leverage not found")
