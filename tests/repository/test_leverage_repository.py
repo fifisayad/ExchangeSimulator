96157f6e9c91e2eb62a9f9933f726f901c7372f3
fifi <sayad.mehrdad@gmail.com>
2025-07-24 14:03:15 +0330

    feat: add test for get all leverage repository

---
+++ tests/repository/test_leverage_repository.py
@@ -0,0 +1,27 @@
+import pytest
+
+from src.repository import LeverageRepository
+from src.enums.market import Market
+from tests.materials import *
+
+
+@pytest.mark.asyncio
+class TestLeverageRepository:
+    leverage_repository = LeverageRepository()
+
+    async def test_get_all_leverages(self, database_provider_test, leverage_factory):
+        leverage_schemas = leverage_factory()
+        leverages = await self.leverage_repository.create_many(data=leverage_schemas)
+
+        assert len(leverage_schemas) == len(leverages)
+
+        got_leverages = await self.leverage_repository.get_all_leverages()
+
+        assert len(got_leverages) == len(leverages)
+
+        leverage_ids = set()
+        for leverage in leverages:
+            leverage_ids.add(leverage.id)
+
+        for leverage in got_leverages:
+            assert leverage.id in leverage_ids
