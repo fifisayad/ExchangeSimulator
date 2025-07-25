41ac68fa18a8cde39e5405ee71334e2d3ce1a114
fifi <sayad.mehrdad@gmail.com>
2025-07-25 12:35:30 +0330

    feat: add fee calc to the order helper

--- src/helpers/order_helper.py
+++ src/helpers/order_helper.py
@@ -1,3 +1,5 @@
+from ..models import Portfolio
+from ..enums.order_type import OrderType
 from ..enums.order_side import OrderSide
 from ..enums.asset import Asset
 from ..enums.market import Market
@@ -15,3 +17,38 @@ class OrderHelper:
             return Asset[second_coin.upper()]
         else:
             return Asset[first_coin.upper()]
+
+    @staticmethod
+    def fee_calc(
+        market: Market,
+        price: float,
+        size: float,
+        side: OrderSide,
+        order_type: OrderType,
+        portfolio: Portfolio,
+    ) -> float:
+        """Calculates and applies trading fees to one or more orders based on
+        order type, side, and whether the market is perpetual or spot.
+        """
+        order_total = size * price
+        if market.is_perptual():
+            if order_type == OrderType.LIMIT:
+                fee = portfolio.perp_maker_fee * order_total
+            elif order_type == OrderType.MARKET:
+                fee = portfolio.perp_taker_fee * order_total
+            else:
+                fee = 0
+        else:
+            if order_type == OrderType.LIMIT:
+                if side == OrderSide.BUY:
+                    fee = size * portfolio.spot_maker_fee
+                else:
+                    fee = order_total * portfolio.spot_maker_fee
+            elif order_type == OrderType.MARKET:
+                if side == OrderSide.BUY:
+                    fee = size * portfolio.spot_taker_fee
+                else:
+                    fee = order_total * portfolio.spot_taker_fee
+            else:
+                fee = 0
+        return fee
