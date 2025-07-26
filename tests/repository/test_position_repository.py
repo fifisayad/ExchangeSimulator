a84f3f9e53ba00707f81bc1fd8bf862ee54087e7
fifi <sayad.mehrdad@gmail.com>
2025-07-26 14:51:32 +0330

    feat: add test for getting positions by portfolio id

--- tests/repository/test_position_repository.py
+++ tests/repository/test_position_repository.py
@@ -1,3 +1,4 @@
+from typing import Set
 import pytest
 
 from src.models import Position
@@ -21,6 +22,34 @@ class TestpositionRepository:
 
         for position in got_positions:
             assert position.id in positions_id_set
+
+    async def test_get_all_positions_with_portfolio_id(
+        self, database_provider_test, position_factory
+    ):
+        portfolio_counts = 5
+        position_counts = 10
+        positions_schemas: List[PositionSchema] = list()
+        positions_portfolio_ids: Set[str] = set()
+        for i in range(portfolio_counts):
+            portfolio_id = str(uuid.uuid4())
+            positions_portfolio_ids.add(portfolio_id)
+            positions_schemas += position_factory(
+                portfolio_id=portfolio_id, count=position_counts
+            )
+        positions: List[Position] = await self.position_repo.create_many(
+            data=positions_schemas
+        )
+        assert len(positions) == portfolio_counts * position_counts
+        LOGGER.info(f"positions counts: {len(positions)}")
+
+        for portfolio_id in positions_portfolio_ids:
+            got_positions = await self.position_repo.get_all_positions(
+                portfolio_id=portfolio_id
+            )
+            assert len(got_positions) == position_counts
+
+            for position in got_positions:
+                assert position.portfolio_id == portfolio_id
 
     async def test_get_all_positions_with_status(
         self, database_provider_test, position_factory
