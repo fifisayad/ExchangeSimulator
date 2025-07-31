3b0e5ed8d746d8838f0a6c3acdb262e466a37816
fifi <sayad.mehrdad@gmail.com>
2025-07-31 22:27:51 +0330

    feat: add test for fill order sucess story

--- tests/engines/test_matching_engine.py
+++ tests/engines/test_matching_engine.py
@@ -2,14 +2,20 @@ import pytest
 
 from src.common.exceptions import InvalidOrder
 from src.engines.matching_engine import MatchingEngine
+from src.enums.order_type import OrderType
 from src.schemas.position_schema import PositionSchema
-from src.services import PositionService
+from src.services import PositionService, OrderService, BalanceService
 from tests.materials import *
+
+
+LOGGER = GetLogger().get()
 
 
 @pytest.mark.asyncio
 class TestMatchingEngine:
     position_service = PositionService()
+    order_service = OrderService()
+    balance_service = BalanceService()
     matching_engine = MatchingEngine()
 
     async def test_perpetual_open_position_check(self, database_provider_test):
@@ -69,3 +75,45 @@ class TestMatchingEngine:
                 side=OrderSide.SELL,
                 size=0.134,
             )
+
+    async def create_portfolio(self):
+        await self.balance_service.create_by_qty(
+            portfolio_id="iamrich", asset=Asset.BTC, qty=0.005
+        )
+        await self.balance_service.create_by_qty(
+            portfolio_id="iamrich", asset=Asset.USD, qty=2000
+        )
+
+    async def test_fill_order_sucess_story(self, database_provider_test):
+        await self.create_portfolio()
+        await self.balance_service.lock_balance(
+            portfolio_id="iamrich", asset=Asset.USD, locked_qty=300
+        )
+        order_schema = OrderSchema(
+            portfolio_id="iamrich",
+            market=Market.BTCUSD,
+            price=1000,
+            size=0.25,
+            side=OrderSide.BUY,
+            fee=0.00045,
+            type=OrderType.LIMIT,
+            status=OrderStatus.ACTIVE,
+        )
+        order = await self.order_service.create(data=order_schema)
+        await self.matching_engine.fill_order(order)
+
+        updated_order = await self.order_service.read_by_id(id_=order.id)
+        assert updated_order is not None
+        assert updated_order.status == OrderStatus.FILLED
+
+        btc_balance = await self.balance_service.read_by_asset(
+            portfolio_id="iamrich", asset=Asset.BTC
+        )
+        assert btc_balance is not None
+        assert btc_balance.available == 0.005 + 0.25 - 0.00045
+
+        usd_balance = await self.balance_service.read_by_asset(
+            portfolio_id="iamrich", asset=Asset.USD
+        )
+        assert usd_balance is not None
+        assert usd_balance.available == 1700
