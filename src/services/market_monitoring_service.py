e86312aed9171ac2c97d51627dc807a217484b9f
fifi <sayad.mehrdad@gmail.com>
2025-08-03 14:19:09 +0330

    feat: complete market monitoring service and its connection

--- src/services/market_monitoring_service.py
+++ src/services/market_monitoring_service.py
@@ -1,6 +1,10 @@
-from typing import Any, Dict, Literal, Optional, Union, overload
+import httpx
+from typing import Dict, Optional, Union, overload
 from fifi import RedisSubscriber, singleton, GetLogger
 
+from ..enums.data_type import DataType
+from ..schemas.subscritption_schema import SubscriptionRequestSchema
+from ..common.exceptions import APIError
 from ..enums.market import Market
 from ..common.settings import Setting
 
@@ -10,25 +14,53 @@ LOGGER = GetLogger().get()
 
 @singleton
 class MarketMonitoringService:
+    trades: Dict[Market, float]
+    subscribers: Dict[Market, RedisSubscriber]
+
     def __init__(self):
-        self.trades: Dict[Market, float] = dict()
+        self.setting = Setting()
+        self.trades = dict()
+        self.subscribers = dict()
+
+    async def start(self):
         LOGGER.info("Monitoring Service initializing is started....")
-        self.start()
+        for market in self.setting.ACTIVE_MARKETS:
+            channel = await self.subscribe(market=market, data_type=DataType.TRADES)
+            self.subscribers[market] = await RedisSubscriber.create(channel=channel)
 
-    def start(self):
-        for market in Setting().ACTIVE_MARKETS:
-            # TODO: implement monitoring connection flow
-            pass
+    async def subscribe(self, market: Market, data_type: DataType) -> str:
+        url = f"{self.setting.MM_API_PATH}{self.setting.MM_SUBSCRIPTION_PATH}"
+        subscritption_schema = SubscriptionRequestSchema(
+            exchange=self.setting.MM_EXCHANGE, market=market, data_type=data_type
+        )
+        try:
+            async with httpx.AsyncClient(
+                timeout=httpx.Timeout(10.0, connect=5.0),
+                transport=httpx.AsyncHTTPTransport(retries=3),
+                headers={"Content-Type": "application/json"},
+            ) as client:
+                resp = await client.post(url, json=subscritption_schema.model_dump())
+                resp.raise_for_status()
+                return resp.json()["channel"]
+        except Exception as e:
+            raise APIError(f"Read position failed: {e}")
 
     @overload
-    def get_last_trade(self, market: Market) -> float: ...
+    async def get_last_trade(self, market: Market) -> float: ...
     @overload
-    def get_last_trade(self, market: None = None) -> Dict[Market, float]: ...
+    async def get_last_trade(self, market: None = None) -> Dict[Market, float]: ...
 
-    def get_last_trade(
+    async def get_last_trade(
         self, market: Optional[Market] = None
     ) -> Union[Dict[Market, float], float]:
         """ """
         if market:
+            last_trade = await self.subscribers[market].get_last_message()
+            if last_trade:
+                self.trades[market] = last_trade["price"]
             return self.trades[market]
+        for sub_market in self.subscribers:
+            last_trade = await self.subscribers[sub_market].get_last_message()
+            if last_trade:
+                self.trades[sub_market] = last_trade["price"]
         return self.trades
