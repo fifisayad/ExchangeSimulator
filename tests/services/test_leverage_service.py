76b3cfcd8c67025af881d80093cddd41240402af
fifi <sayad.mehrdad@gmail.com>
2025-08-03 14:39:16 +0330

    feat: add test for getting leverage value

--- tests/services/test_leverage_service.py
+++ tests/services/test_leverage_service.py
@@ -24,3 +24,19 @@ class TestLeverageService:
 
             assert got_leverage is not None
             assert got_leverage.leverage == leverage.leverage
+
+    async def test_get_portfolio_market_leverage_value(
+        self, database_provider_test, leverage_factory
+    ):
+        leverages_schemas = leverage_factory()
+        leverages = await self.leverage_service.create_many(leverages_schemas)
+
+        for leverage in leverages:
+            got_leverage = (
+                await self.leverage_service.get_portfolio_market_leverage_value(
+                    portfolio_id=leverage.portfolio_id, market=leverage.market
+                )
+            )
+
+            assert got_leverage is not None
+            assert got_leverage == leverage.leverage
