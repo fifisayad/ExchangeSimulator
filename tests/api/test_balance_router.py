72548b7c400b760dd1ff905b03a09908900eda64
fifi <sayad.mehrdad@gmail.com>
2025-08-05 18:30:01 +0330

    feat: add test for the deposit balance api

--- tests/api/test_balance_router.py
+++ tests/api/test_balance_router.py
@@ -6,7 +6,8 @@ from main import app
 from fastapi.encoders import jsonable_encoder
 
 from src.services import BalanceService
-from src.schemas.balance_schema import BalanceResponseSchema
+from src.schemas.balance_schema import BalanceDepositSchema, BalanceResponseSchema
+from src.services.portfolio_service import PortfolioService
 from tests.materials import *
 
 LOGGER = GetLogger().get()
@@ -15,6 +16,7 @@ LOGGER = GetLogger().get()
 @pytest.mark.asyncio
 class TestBalanceRouter:
     balance_service = BalanceService()
+    portfolio_service = PortfolioService()
 
     async def create_balance(self, balance_factory_for_portfolios):
         balance_schemas = balance_factory_for_portfolios("iamrich")
@@ -134,3 +136,134 @@ class TestBalanceRouter:
                     mock_method_asset.assert_awaited_once_with(
                         portfolio_id="h1", asset=Asset.BTC
                     )
+
+    async def test_deposit_balance_update(
+        self, database_provider_test, balance_factory_for_portfolios
+    ):
+        balances = await self.create_balance(balance_factory_for_portfolios)
+        balance = balances[-1]
+        with patch.object(
+            BalanceService, "add_balance", return_value=True
+        ) as mock_method_add:
+            with patch.object(
+                BalanceService, "read_by_asset", return_value=balance
+            ) as mock_method_read:
+                async with AsyncClient(
+                    transport=ASGITransport(app=app), base_url="http://test/exapi/v1"
+                ) as ac:
+                    response = await ac.patch(
+                        f"/balance/deposit",
+                        json=jsonable_encoder(
+                            BalanceDepositSchema(
+                                portfolio_id=balance.portfolio_id,
+                                asset=Asset.BTC,
+                                quantity=0.1,
+                            )
+                        ),
+                    )
+                    assert response.status_code == 200
+                    LOGGER.info(f"balance response: {response.json()}")
+                    assert response.json() == jsonable_encoder(
+                        BalanceResponseSchema(**balance.to_dict())
+                    )
+                    mock_method_add.assert_awaited_once_with(
+                        portfolio_id=balance.portfolio_id,
+                        asset=Asset.BTC,
+                        qty=0.1,
+                    )
+                    mock_method_read.assert_awaited_once_with(
+                        portfolio_id=balance.portfolio_id, asset=Asset.BTC
+                    )
+
+    async def test_deposit_balance_create(
+        self, database_provider_test, balance_factory_for_portfolios
+    ):
+        balances = await self.create_balance(balance_factory_for_portfolios)
+        portfolio = await self.portfolio_service.create(
+            data=PortfolioSchema(name="iamrich")
+        )
+        balance = balances[-1]
+        with patch.object(
+            BalanceService, "add_balance", return_value=False
+        ) as mock_method_add:
+            with patch.object(
+                PortfolioService, "read_by_id", return_value=portfolio
+            ) as mock_method_portfolio_read:
+                with patch.object(
+                    BalanceService, "create_by_qty", return_value=balance
+                ) as mock_method_create:
+                    async with AsyncClient(
+                        transport=ASGITransport(app=app),
+                        base_url="http://test/exapi/v1",
+                    ) as ac:
+                        response = await ac.patch(
+                            f"/balance/deposit",
+                            json=jsonable_encoder(
+                                BalanceDepositSchema(
+                                    portfolio_id=balance.portfolio_id,
+                                    asset=Asset.BTC,
+                                    quantity=0.1,
+                                )
+                            ),
+                        )
+                        assert response.status_code == 200
+                        LOGGER.info(f"balance response: {response.json()}")
+                        assert response.json() == jsonable_encoder(
+                            BalanceResponseSchema(**balance.to_dict())
+                        )
+                        mock_method_add.assert_awaited_once_with(
+                            portfolio_id=balance.portfolio_id,
+                            asset=Asset.BTC,
+                            qty=0.1,
+                        )
+                        mock_method_portfolio_read.assert_awaited_once_with(
+                            id_=balance.portfolio_id
+                        )
+                        mock_method_create.assert_awaited_once_with(
+                            portfolio_id=balance.portfolio_id,
+                            asset=Asset.BTC,
+                            qty=0.1,
+                        )
+
+    async def test_create_balance_failed(
+        self, database_provider_test, balance_factory_for_portfolios
+    ):
+        balances = await self.create_balance(balance_factory_for_portfolios)
+        portfolio = await self.portfolio_service.create(
+            data=PortfolioSchema(name="iamrich")
+        )
+        balance = balances[-1]
+        with patch.object(
+            BalanceService, "add_balance", return_value=False
+        ) as mock_method_add:
+            with patch.object(
+                PortfolioService, "read_by_id", return_value=None
+            ) as mock_method_portfolio_read:
+                with patch.object(
+                    BalanceService, "create_by_qty", return_value=balance
+                ) as mock_method_create:
+                    async with AsyncClient(
+                        transport=ASGITransport(app=app),
+                        base_url="http://test/exapi/v1",
+                    ) as ac:
+                        response = await ac.patch(
+                            f"/balance/deposit",
+                            json=jsonable_encoder(
+                                BalanceDepositSchema(
+                                    portfolio_id=balance.portfolio_id,
+                                    asset=Asset.BTC,
+                                    quantity=0.1,
+                                )
+                            ),
+                        )
+                        assert response.status_code == 400
+                        LOGGER.info(f"balance response: {response.json()}")
+                        mock_method_add.assert_awaited_once_with(
+                            portfolio_id=balance.portfolio_id,
+                            asset=Asset.BTC,
+                            qty=0.1,
+                        )
+                        mock_method_portfolio_read.assert_awaited_once_with(
+                            id_=balance.portfolio_id
+                        )
+                        mock_method_create.assert_not_awaited()
