f50e35d2e7eadfa543052959a5f806faefd15295
fifi <sayad.mehrdad@gmail.com>
2025-08-13 16:10:33 +0330

    fix: tests for sqllite integration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 import sqlite3
 import pytest
 
@@ -8,6 +9,10 @@ from fifi import DatabaseProvider
 @pytest.fixture
 def database_provider_test():
     sqlite3.connect("memory")
+    os.environ["DATABASE_HOST"] = ""
+    os.environ["DATABASE_PORT"] = "0"
+    os.environ["DATABASE_USER"] = ""
+    os.environ["DATABASE_PASS"] = ""
     db = DatabaseProvider(
         user="",
         password="",
@@ -17,6 +22,8 @@ def database_provider_test():
         db_tech="sqlite",
         db_lib="aiosqlite",
     )
+    event_loop = asyncio.new_event_loop()
+    event_loop.run_until_complete(db.init_models())
     yield db
     # remove singleton instance
     DatabaseProvider.instance = None
