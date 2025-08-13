077c8a872413500ac2096e7b22d8d177ed83f9b6
fifi <sayad.mehrdad@gmail.com>
2025-08-13 20:59:48 +0330

    fix: check type in redis channel message

--- src/services/market_monitoring_service.py
+++ src/services/market_monitoring_service.py
@@ -68,10 +68,12 @@ class MarketMonitoringService:
         if market:
             last_trade = await self.subscribers[market].get_last_message()
             if last_trade:
-                self.trades[market] = last_trade["data"]["price"]
+                if last_trade["type"] == "trades":
+                    self.trades[market] = last_trade["data"]["price"]
             return self.trades[market]
         for sub_market in self.subscribers:
             last_trade = await self.subscribers[sub_market].get_last_message()
             if last_trade:
-                self.trades[sub_market] = last_trade["data"]["price"]
+                if last_trade["type"] == "trades":
+                    self.trades[sub_market] = last_trade["data"]["price"]
         return self.trades
